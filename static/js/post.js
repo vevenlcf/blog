752b7290911c17d4dd83b82a2d9cca9bce4803b0
vevenlcf <770722426@qq.com>
2019-07-10 10:36:52 +0800

    Update post.js
    修正 字符过长 导致gitment 评论 失败

--- static/js/post.js
+++ static/js/post.js
@@ -30,7 +30,8 @@ function share(){
 
 function gitment() {
     var gitment = new Gitment({
-        id: window.location.pathname,
+        //id: window.location.pathname,
+        id: '<%= page.date %>'
         owner: '{{site.github.username}}',
         repo: '{{site.gitment.repo}}',
         oauth: {
